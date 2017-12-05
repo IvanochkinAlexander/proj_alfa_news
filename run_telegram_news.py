bb74c36f8e4fa4c9357e39cdd1db61ca52297d1a
IvanochkinAlexander <kasperrrok@gmail.com>
2017-12-05 16:07:45 +0100

    [+] fixed bugs

--- run_telegram_news.py
+++ run_telegram_news.py
@@ -47,7 +47,8 @@ def get_links (t, l):
     browser.set_window_size(1120, 550)
     browser.get("https://duckduckgo.com/?q={}".format(t)+ "&ar=news&ia=news&kl={}".format(l))
     browser.find_element_by_id('search_form_input').send_keys(t)
-    NEXT_BUTTON_XPATH = '//*[@id="links_wrapper"]/div[2]/div/a'
+    # NEXT_BUTTON_XPATH = '//*[@id="links_wrapper"]/div[2]/div/a'
+    NEXT_BUTTON_XPATH ='//*[@id="links_wrapper"]/div[1]/div[2]/div/a'
     button = browser.find_element_by_xpath(NEXT_BUTTON_XPATH)
     button.click()
     page = browser.page_source
@@ -90,6 +91,7 @@ def parse_links(links_df):
     texts = []
     links = []
     text_df = pd.DataFrame()
+
     for i in links_df['url']:
         try:
             a = Article(i, language='en')
@@ -219,77 +221,198 @@ def find_duplicates (df_text):
     """
     Count cosine similarity on TF-IDF vectors
     """
-
-    dictionary = corpora.Dictionary(df_text)
-    corpus = [dictionary.doc2bow(t) for t in df_text]
-    tfidf = models.TfidfModel(corpus)
-    corpus_tfidf = tfidf[corpus]
-    index = similarities.MatrixSimilarity(tfidf[corpus])
-    sims = index[corpus_tfidf]
-    sims_df = pd.DataFrame(sims)
-    for i in range(len(df_text)):
-        sims_df.iloc[i,i] = -1
-    cols = sims_df.columns
-    sims_df['max_text'] = sims_df.apply(lambda x: sims_df.columns[x == x.max()][0], axis=1)
-    sims_df['max_text_proba'] = sims_df[cols].max(axis=1)
-    return sims_df[['max_text', 'max_text_proba']]
+    sims_df = pd.DataFrame()
+    try:
+        dictionary = corpora.Dictionary(df_text)
+        corpus = [dictionary.doc2bow(t) for t in df_text]
+        tfidf = models.TfidfModel(corpus)
+        corpus_tfidf = tfidf[corpus]
+        index = similarities.MatrixSimilarity(tfidf[corpus])
+        sims = index[corpus_tfidf]
+        sims_df = pd.DataFrame(sims)
+        for i in range(len(df_text)):
+            sims_df.iloc[i,i] = -1
+        cols = sims_df.columns
+        sims_df['max_text'] = sims_df.apply(lambda x: sims_df.columns[x == x.max()][0], axis=1)
+        sims_df['max_text_proba'] = sims_df[cols].max(axis=1)
+        return sims_df[['max_text', 'max_text_proba']]
+    except:
+        return sims_df
+    # pass
 
 def del_interntal_duplicates(concated_cut):
 
     """
     Delete duplicates using cosine similarity on TF-IDF
     vectors not to allow send similiar news about banks
-    """
 
-    sims = find_duplicates (concated_cut['text'].apply(review_to_wordlist))
-    concated_sims = pd.concat([concated_cut.reset_index(drop=True), sims], axis=1)
-    more_df = concated_sims[concated_sims['max_text_proba']>=0.6]
-    less_df = concated_sims[concated_sims['max_text_proba']<0.6]
-    more_df = more_df.drop_duplicates(subset='max_text_proba', keep='first')
-    concated_sims  = pd.concat([less_df, more_df])
-    del concated_sims['max_text_proba']
-    del concated_sims['max_text']
-    return concated_sims
+    """
+    concated_sims = pd.DataFrame()
+    try:
+        sims = find_duplicates (concated_cut['text'].apply(review_to_wordlist))
+        concated_sims = pd.concat([concated_cut.reset_index(drop=True), sims], axis=1)
+        more_df = concated_sims[concated_sims['max_text_proba']>=0.6]
+        less_df = concated_sims[concated_sims['max_text_proba']<0.6]
+        more_df = more_df.drop_duplicates(subset='max_text_proba', keep='first')
+        concated_sims  = pd.concat([less_df, more_df])
+        del concated_sims['max_text_proba']
+        del concated_sims['max_text']
+        return concated_sims
+    except:
+        return concated_sims
+
+# to_search = [
+#      'новый продукт',
+#      'новые условия',
+#      'впервые',
+#      'первые на рынок',
+#      'банк представить',
+#      'выводить на рынок',
+#      'любое отделение',
+#      'любой сотрудник',
+#      'любой клиент',
+#      'без очереди',
+#      'любой специалист',
+#      'без ожидание',
+#      'любой время',
+#      'один документ',
+#      'два документа',
+#      'новый технология',
+#      'новый процесс',
+#      'появление возможность',
+#      'становиться быстрее',
+#      'новый возможность',
+#      'инновация',
+#      'сокращение',
+#      'инновационный',
+#      'начинать',
+#      'появиться',
+#      'альфа-банк',
+#      'альфабанк',
+#      'альфа банк'
+#     'тинькофф',
+#     'сбербанк',
+#     'модульбанк',
+#     'газпромбанк',
+#     'точка банк',
+#     'втб',
+#     'райффайзен',
+#     'юникредит',
+#     'фк открытие',
+#     'бинбанк','new product',
+# 'new conditions',
+# 'first on the market',
+# 'bank to present',
+# 'bring to the market',
+# 'any department',
+# 'any employee',
+# 'any customer',
+# 'no turn',
+# 'any specialist',
+# 'no waiting',
+# 'any time',
+# 'one document',
+# 'two documents',
+# 'new technology',
+# 'new process',
+# 'get faster',
+# 'faster',
+# 'better',
+# 'new opportunity',
+# 'innovation',
+# 'innovative',
+# 'start up',
+# 'machine learning',
+# 'biometric technology',
+# 'new technology',
+# 'big data',
+# 'robotic technology',
+# 'bank of america',
+# 'royal bank of canada',
+# 'industrial and commercial bank of china',
+# 'citibank',
+# 'societe generale',
+# 'standard chartered bank',
+# 'sorthern trust',
+# 'maybank islamic',
+# 'ubs',
+# 'citibank',
+# 'bnp paribas',
+# 'jp morgan',
+# 'bank of america',
+# 'ing',
+# 'raiffeisen',
+# 'santander',
+# 'icbc',
+# 'arab bank',
+# 'standard bank',
+# 'lead off'
+# 'premier'
+# 'leading-edge'
+# 'breaking new ground'
+# 'state-of-the-art'
+# 'progressive',
+# 'latest',
+# 'brand-new',
+# 'unique',
+# 'leading',
+# 'machine',
+# 'robotics',
+# 'hi tech',
+# 'scientific',
+# 'digital',
+# 'artificial',
+# 'intelligence'
+# ]
+
 
 to_search = [
-     'новый продукт',
-     'новые условия',
-     'впервые',
-     'первые на рынок',
-     'банк представить',
-     'выводить на рынок',
-     'любое отделение',
-     'любой сотрудник',
-     'любой клиент',
-     'без очереди',
-     'любой специалист',
-     'без ожидание',
-     'любой время',
-     'один документ',
-     'два документа',
-     'новый технология',
-     'новый процесс',
-     'появление возможность',
-     'становиться быстрее',
-     'новый возможность',
-     'инновация',
-     'сокращение',
-     'инновационный',
-     'начинать',
-     'появиться',
-     'альфа-банк',
-     'альфабанк',
-     'альфа банк'
-    'тинькофф',
-    'сбербанк',
-    'модульбанк',
-    'газпромбанк',
-    'точка банк',
-    'втб',
-    'райффайзен',
-    'юникредит',
-    'фк открытие',
-    'бинбанк','new product',
+'ДБО',
+'лицензия',
+'крипто',
+'юрлица',
+'акции',
+'банковских карт',
+'карта',
+'запустила',
+'новый продукт',
+'новые условия',
+'впервые',
+'первые на рынок',
+'банк представить',
+'выводить на рынок',
+'любое отделение',
+'любой сотрудник',
+'любой клиент',
+'без очереди',
+'любой специалист',
+'без ожидание',
+'любой время',
+'один документ',
+'два документа',
+'новый технология',
+'новый процесс',
+'появление возможность',
+'становиться быстрее',
+'новый возможность',
+'инновация',
+'сокращение',
+'инновационный',
+'начинать',
+'появиться',
+'альфа-банк',
+'альфабанк',
+'альфа банк'
+'тинькофф',
+'сбербанк',
+'модульбанк',
+'газпромбанк',
+'точка банк',
+'втб',
+'райффайзен',
+'юникредит',
+'фк открытие',
+'бинбанк','new product',
 'new conditions',
 'first on the market',
 'bank to present',
@@ -355,6 +478,7 @@ to_search = [
 'intelligence'
 ]
 
+
 browser = webdriver.PhantomJS(executable_path='/../usr/local/bin/phantomjs')
 morph = MorphAnalyzer()
 russian_stops = stopwords.words('russian')
@@ -377,25 +501,29 @@ def run_russia ():
             pass
         print ('длина составляет {}'.format(links_df.shape[0]))
 
-    if l =='ru-ru':
-        text_df = parse_links_ru(links_df)
+    if links_df.shape[0]==0:
+        pass
     else:
-        text_df = parse_links(links_df)
 
-    concated_df = find_keywords(text_df, links_df)
-    concated_exclude, temp_exclude = to_exclude_text(concated_df)
-    concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
-    concated_sims = del_interntal_duplicates(concated_cut)
-    parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
-    parsed_links = parsed_df['link']
-    concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
-    concated_sims['country'] = c
-    for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
-        send_to_telegram(i, m, c)
-        time.sleep(5)
-    parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
-    parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
-    print ('finished iteration')
+        if l =='ru-ru':
+            text_df = parse_links_ru(links_df)
+        else:
+            text_df = parse_links(links_df)
+
+        concated_df = find_keywords(text_df, links_df)
+        concated_exclude, temp_exclude = to_exclude_text(concated_df)
+        concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
+        concated_sims = del_interntal_duplicates(concated_cut)
+        parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
+        parsed_links = parsed_df['link']
+        concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
+        concated_sims['country'] = c
+        for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
+            send_to_telegram(i, m, c)
+            time.sleep(5)
+        parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
+        parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
+        print ('finished iteration')
 
 
 def run_england ():
@@ -413,26 +541,29 @@ def run_england ():
         except:
             pass
         print ('длина составляет {}'.format(links_df.shape[0]))
+        if links_df.shape[0]==0:
+            pass
+        else:
 
-    if l =='ru-ru':
-        text_df = parse_links_ru(links_df)
-    else:
-        text_df = parse_links(links_df)
+            if l =='ru-ru':
+                text_df = parse_links_ru(links_df)
+            else:
+                text_df = parse_links(links_df)
 
-    concated_df = find_keywords(text_df, links_df)
-    concated_exclude, temp_exclude = to_exclude_text(concated_df)
-    concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
-    concated_sims = del_interntal_duplicates(concated_cut)
-    parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
-    parsed_links = parsed_df['link']
-    concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
-    concated_sims['country'] = c
-    for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
-        send_to_telegram(i, m, c)
-        time.sleep(5)
-    parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
-    parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
-    print ('finished iteration')
+            concated_df = find_keywords(text_df, links_df)
+            concated_exclude, temp_exclude = to_exclude_text(concated_df)
+            concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
+            concated_sims = del_interntal_duplicates(concated_cut)
+            parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
+            parsed_links = parsed_df['link']
+            concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
+            concated_sims['country'] = c
+            for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
+                send_to_telegram(i, m, c)
+                time.sleep(5)
+            parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
+            parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
+            print ('finished iteration')
 
 
 
@@ -452,25 +583,29 @@ def run_usa ():
             pass
         print ('длина составляет {}'.format(links_df.shape[0]))
 
-    if l =='ru-ru':
-        text_df = parse_links_ru(links_df)
+    if links_df.shape[0]==0:
+        pass
     else:
-        text_df = parse_links(links_df)
 
-    concated_df = find_keywords(text_df, links_df)
-    concated_exclude, temp_exclude = to_exclude_text(concated_df)
-    concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
-    concated_sims = del_interntal_duplicates(concated_cut)
-    parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
-    parsed_links = parsed_df['link']
-    concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
-    concated_sims['country'] = c
-    for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
-        send_to_telegram(i, m, c)
-        time.sleep(5)
-    parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
-    parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
-    print ('finished iteration')
+        if l =='ru-ru':
+            text_df = parse_links_ru(links_df)
+        else:
+            text_df = parse_links(links_df)
+
+        concated_df = find_keywords(text_df, links_df)
+        concated_exclude, temp_exclude = to_exclude_text(concated_df)
+        concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
+        concated_sims = del_interntal_duplicates(concated_cut)
+        parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
+        parsed_links = parsed_df['link']
+        concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
+        concated_sims['country'] = c
+        for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
+            send_to_telegram(i, m, c)
+            time.sleep(5)
+        parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
+        parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
+        print ('finished iteration')
 
 def run_india ():
 
@@ -487,26 +622,33 @@ def run_india ():
         except:
             pass
         print ('длина составляет {}'.format(links_df.shape[0]))
+    if links_df.shape[0]==0:
+        pass
 
-    if l =='ru-ru':
-        text_df = parse_links_ru(links_df)
     else:
-        text_df = parse_links(links_df)
+        if l =='ru-ru':
+            text_df = parse_links_ru(links_df)
+        else:
+            text_df = parse_links(links_df)
 
-    concated_df = find_keywords(text_df, links_df)
-    concated_exclude, temp_exclude = to_exclude_text(concated_df)
-    concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
-    concated_sims = del_interntal_duplicates(concated_cut)
-    parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
-    parsed_links = parsed_df['link']
-    concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
-    concated_sims['country'] = c
-    for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
-        send_to_telegram(i, m, c)
-        time.sleep(5)
-    parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
-    parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
-    print ('finished iteration')
+        concated_df = find_keywords(text_df, links_df)
+        concated_exclude, temp_exclude = to_exclude_text(concated_df)
+        concated_cut = concated_exclude[(concated_exclude[0]!='') & (~concated_exclude[0].isnull())].reset_index(drop=True)
+        try:
+            concated_sims = del_interntal_duplicates(concated_cut)
+            parsed_df = pd.read_excel('../output/alfa_news/parsed.xlsx')
+            parsed_links = parsed_df['link']
+            concated_sims = concated_sims[~concated_sims['link'].isin(parsed_links)]
+            concated_sims['country'] = c
+            for i, m, c in zip ( concated_sims[0], concated_sims['link'], concated_sims['country']):
+                send_to_telegram(i, m, c)
+                time.sleep(5)
+            parsed_df_new = pd.concat([parsed_df, concated_sims[['link', 'text']]], axis=0)
+            parsed_df_new.to_excel('../output/alfa_news/parsed.xlsx')
+            print ('finished iteration')
+        except:
+            print ('что-то пошло не так')
+            pass
 
 
 ## очистить результаты
@@ -521,6 +663,7 @@ schedule.every().day.at("8:00").do(run_russia)
 schedule.every().day.at("11:00").do(run_england)
 schedule.every().day.at("16:00").do(run_usa)
 schedule.every().day.at("5:30").do(run_india)
+# schedule.every().day.at("7:31").do(run_india)
 
 
 # schedule.every().day.at("19:27").do(run_russia)
